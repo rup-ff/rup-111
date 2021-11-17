db9d9c58597269bf45abbc814c9e20459fe56abc
rup-ff <86917835+rup-ff@users.noreply.github.com>
2021-11-17 15:39:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+Webcam.set({
+ width:350,
+ height:300,
+ image_format:'png',
+ png_quality:90
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+   Webcam.snap(function(data_uri)
+   {
+     document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>'
+   });
+}
+console.log('ml5 version:' , ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/4tmoHsrhb/.json',modelLoaded);
+function modelLoaded()
+{
+console.log('Model Loaded!');
+}
+function speak ()
+{
+ var synth = window.SpeechSynthesis;
+ speak_data_1 = "THE FIRST PREDICTION IS" + prediction_1;
+ speak_data_2= " AND THE SECOND PREDICTION IS" + prediction_2;
+ var utterThis = new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+ synth.speak(utterThis);
+}
